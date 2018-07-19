4e6583925d135dcdb3858fad3ec7259cef5540de
akhilsoni06 <soni.akhil06@gmail.com>
2018-07-20 00:34:43 +0530

    intital commit

---
+++ app/src/main/java/com/example/akhil/pizzaapplication/PizzaListAdapter.java
@@ -0,0 +1,50 @@
+package com.example.akhil.pizzaapplication;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class PizzaListAdapter extends RecyclerView.Adapter<PizzaListAdapter.ItemHolder> {
+    public PizzaListAdapter() {
+    }
+
+    @NonNull
+    @Override
+    public ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.pizza_list_item_layoyt, parent, false);
+        return new ItemHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemHolder holder, int position) {
+        holder.mTxtPizzaName.setText("pos=" + position);
+        holder.mTxtPizzaType.setText("type=" + position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 100;
+    }
+
+
+    public static class ItemHolder extends RecyclerView.ViewHolder {
+
+        TextView mTxtPizzaName;
+        TextView mTxtPizzaType;
+
+        public ItemHolder(View itemView) {
+            super(itemView);
+            mTxtPizzaName = (TextView) itemView.findViewById(R.id.pizza_name);
+            mTxtPizzaType = (TextView) itemView.findViewById(R.id.txt_pizza_size);
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/akhil/pizzaapplication/PizzaListFragment.java
@@ -0,0 +1,40 @@
+package com.example.akhil.pizzaapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PizzaListFragment extends Fragment {
+
+    private RecyclerView mListPizza;
+    private LinearLayoutManager mLinearLayoutManager;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list_layout, container, false);
+        initViews(view);
+        return view;
+    }
+
+
+    private void initViews(View view) {
+        mListPizza = (RecyclerView) view.findViewById(R.id.list_pizza);
+        mLinearLayoutManager = new LinearLayoutManager(getActivity());
+        mLinearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mListPizza.setLayoutManager(mLinearLayoutManager);
+        setAdapter();
+    }
+
+
+    private void setAdapter() {
+        PizzaListAdapter adapter = new PizzaListAdapter();
+        mListPizza.setAdapter(adapter);
+    }
+}
